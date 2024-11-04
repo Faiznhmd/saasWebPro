87aca9045c79733b38fd402cb795660a7497daab
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-11-04 15:07:11 +0530

    craeteurl

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,42 @@ export function cn(...inputs: ClassValue[]) {
 export function removeTrailingSlash(path: string) {
   return path.replace(/\/$/, '');
 }
+
+// export function createURL(
+//   href: string,
+//   oldParams: Record<string, string>,
+//   newParams: Record<string, string | undefined>
+// ) {
+//   const params = new URLSearchParams(oldParams);
+//   Object.entries(newParams).forEach(([key, value]) => {
+//     if (value == undefined) {
+//       params.delete(key);
+//     } else {
+//       params.set(key, value);
+//     }
+//   });
+//   return `${href}?${params.toString()}`;
+// }
+
+export function createURL(
+  href: string,
+  oldParams: Record<string, string>,
+  newParams: Record<string, string | undefined>
+) {
+  // Filter out any non-string values in oldParams
+  const filteredOldParams = Object.fromEntries(
+    Object.entries(oldParams).filter(([, value]) => typeof value === 'string')
+  );
+
+  const params = new URLSearchParams(filteredOldParams);
+
+  Object.entries(newParams).forEach(([key, value]) => {
+    if (value == undefined) {
+      params.delete(key);
+    } else {
+      params.set(key, value);
+    }
+  });
+
+  return `${href}?${params.toString()}`;
+}
